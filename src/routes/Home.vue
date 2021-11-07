8e215b7ffb96dccb215c6b876d4d2ce420565782
kwonsean <gownn12@naver.com>
2021-11-07 17:45:03 +0900

    feat: Add search component
    - Search 컴포넌트를 추가하였습니다.

--- src/routes/Home.vue
+++ src/routes/Home.vue
@@ -1,12 +1,15 @@
 <template>
   <Headline />
+  <Search />
 </template>
 
 <script>
 import Headline from '~/components/Headline.vue'
+import Search from '~/components/Search.vue'
 export default {
   components:{
-    Headline
+    Headline,
+    Search
   }
 }
 </script>

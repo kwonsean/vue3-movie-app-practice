2dd0bd09add10fc01eae7163b65355d900f742eb
kwonsean <gownn12@naver.com>
2021-11-07 15:38:49 +0900

    feat: Add TheHeader
    - 헤더 부분 컴포넌트를 작성하고 App.vue에 연결하였습니다.
    - active-class의  기본값은 router-link-active인데 이를 active로 변경하여
      부트스트랩에서 사용되도록 함

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,16 @@
 <template>
+  <TheHeader />
   <RouterView />
 </template>
 
+<script>
+import TheHeader from '~/components/TheHeader.vue'
+export default {
+  components:{
+    TheHeader
+  }
+}
+</script>
 
 <style lang="scss">
 // 모든 컴포넌트에서 부트스르랩을 사용하기 위해 scoped는 작성하지 않는다.

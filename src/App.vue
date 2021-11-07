f42d036bd2e6bf86c8cbb7fb052526bc82bcf873
kwonsean <gownn12@naver.com>
2021-11-07 15:15:08 +0900

    feat: Set default bootstrap primary color
    - 기본색을 지정하고 이를 테스트하는 코드를 작성하였습니다.

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,9 @@
 <template>
   <RouterView />
 </template>
+
+
+<style lang="scss">
+// 모든 컴포넌트에서 부트스르랩을 사용하기 위해 scoped는 작성하지 않는다.
+@import "~/scss/main.scss"
+</style>
--- src/routes/Home.vue
+++ src/routes/Home.vue
@@ -1,3 +1,6 @@
 <template>
   <h1>HOME!</h1>
+  <div class="btn btn-primary">
+    HOME
+  </div>
 </template>

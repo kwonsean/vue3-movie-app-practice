6e8ded33ac2be4cb12b7590642ee6bc1d9bd90db
kwonsean <gownn12@naver.com>
2021-11-09 00:13:52 +0900

    feat: Add about page
    - about page를 생성

--- src/routes/About.vue
+++ src/routes/About.vue
@@ -1,3 +1,90 @@
 <template>
-  <h1>ABOUT!</h1>
+  <div class="about">
+    <div class="photo">
+      <Loader
+        v-if="imageLoading"
+        absolute />
+      <img
+        :src="image"
+        alt="soul image" />
+    </div>
+    <div class="name">
+      {{ name }}
+    </div>
+    <div>{{ email }}</div>
+    <div>{{ blog }}</div>
+    <div>{{ phone }}</div>
+  </div>
 </template>
+
+<script>
+import Loader from '~/components/Loader'
+export default {
+  components: {
+    Loader
+  },
+  data() {
+    return {
+      imageLoading: true
+    }
+  },
+  computed:{
+    image () {
+      return this.$store.state.about.image
+    },
+    name() {
+      return this.$store.state.about.name
+    },
+    email() {
+      return this.$store.state.about.email
+    },
+    blog() {
+      return this.$store.state.about.blog
+    },
+    phone() {
+      return this.$store.state.about.phone
+    },
+  },
+  mounted(){
+    this.init()
+  },
+  methods: {
+    async init() {
+      await this.$loadImage(this.image)
+      this.imageLoading = false
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@import "~/scss/main.scss";
+.about{
+  text-align: center;
+  .photo{
+    position: relative;
+    width: 250px;
+    height: 250px;
+    margin: 40px auto 20px;
+    padding: 30px;
+    border: 10px solid $gray-300;
+    border-radius: 50%;
+    box-sizing: border-box;
+    background-color: $gray-200;
+    img{
+    width: 180px;
+    height: 180px;
+    position: absolute;
+    top: 25px;
+    left: 25px;
+    border-radius: 50%;
+    border: 2px solid $gray-600;
+    }
+  }
+  .name {
+    font-size: 40px;
+    font-family: "Oswald", sans-serif;
+    margin-bottom: 20px;
+  }
+}
+</style>
---
+++ src/store/about.js
@@ -0,0 +1,10 @@
+export default {
+  namespaced: true,
+  state: () => ({
+    name: 'KSH',
+    email: 'kshwbft@gmail.com',
+    blog: 'https://kwonsean.tistory.com/',
+    phone: '+82-10-1234-5678',
+    image: 'https://search.pstatic.net/common/?src=http%3A%2F%2Fblogfiles.naver.net%2FMjAyMTAxMjRfMjc1%2FMDAxNjExNDk5NzU3OTM0.kGlkJay8gQVqanjoPSQylQL5zOnZHqdmHlAMtmYoBEQg.h-0BKUxvOGt80qI8yRtFYzUrjGkJxCQuz7NbnqvsM5Qg.JPEG.crispin2533%2F22%25B9%25F8.jpg&type=sc960_832'
+  })
+}

35097a8c7401bf56f5f120ece1d371a9bbdb238e
kwonsean <gownn12@naver.com>
2021-11-08 16:07:35 +0900

    feat: Use store state
    - 스토어에 저장한 데이터를 사용함

--- src/components/MovieList.vue
+++ src/components/MovieList.vue
@@ -1,9 +1,10 @@
 <template>
-  <div class="continer">
+  <div class="container">
     <div class="inner">
       <MovieItem
         v-for="movie in movies"
-        :key="movie.imdbID" />
+        :key="movie.imdbID" 
+        :movie="movie" />
     </div>
   </div>
 </template>
@@ -14,9 +15,9 @@ export default {
   components:{
     MovieItem
   },
-  data(){
-    return{
-      movies: []
+  computed:{
+    movies() {
+      return this.$store.state.movie.movies
     }
   }
 }

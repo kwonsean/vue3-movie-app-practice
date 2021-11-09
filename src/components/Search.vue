d69768a9c8dc2c2779d1a32feeddb4cbdc0c94da
kwonsean <gownn12@naver.com>
2021-11-09 13:39:07 +0900

    feat: Add media-breakpoint-down
    - 반응형 웹을위해 breakpoint를 사용했습니다.

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -77,6 +77,7 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+@import "~/scss/main";
 .container{
   display: flex;
   > * { // 전체 자식 요소 선택
@@ -102,5 +103,23 @@ export default {
     font-weight: 700;
     flex-shrink: 0; // 크기가 지정된 너비보다 줄어들지 않도록
   }
+  @include media-breakpoint-down(lg){
+    display: block;
+    input{
+    margin-right: 0;
+    margin-bottom: 10px;
+    }
+    .selects{
+      margin-right: 0;
+      margin-bottom: 10px;
+      select{
+        width: 100%;
+      }
+    }
+    .btn {
+      width: 100%;
+    }
+  }
 }
+
 </style>
--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -15,6 +15,13 @@
         </RouterLink>
       </div>
     </div>
+    <div
+      class="user"
+      @click="toAbout">
+      <img
+        :src="image"
+        :alt="name" />
+    </div>
   </header>
 </template>
 
@@ -44,24 +51,68 @@ export default {
       ]
     }
   },
+  computed:{
+    image(){
+      return this.$store.state.about.image
+    },
+    name() {
+      return this.$store.state.about.name
+    }
+  },
   methods: {
     isMatch(path){
       if (!path) return false // path값이 없으면 false 리턴 (active 따로 추가 x)
       console.log(this.$route)
       return path.test(this.$route.fullPath) // 정규표현식 path 조건에 지금 현재 path가 통과하는지 확인 
+    },
+    toAbout(){
+      this.$router.push('/about')
     }
   }
 }
 </script>
 
 <style lang="scss" scoped>
+@import "~/scss/main";
   header{
+    position: relative;
     height: 70px;
     padding: 0 40px;
     display: flex;
     align-items: center;
     .logo{
       margin-right: 40px;
+    }
+    .user{
+      width: 40px;
+      height: 40px;
+      padding: 6px;
+      border-radius: 50%;
+      box-sizing: border-box;
+      background-color: $gray-200;
+      cursor: pointer;
+      position: absolute;
+      top: 0;
+      bottom: 0;
+      right: 40px;
+      margin: auto;
+      transition: .4s;
+      &:hover{
+        background-color: darken($gray-200, 10%);
+      }
+      img{
+        width: 30px;
+        height: 30px;
+        border-radius: 50%;
+        position: absolute;
+        top: 5px;
+        left: 5px;
+      }
+    }
+    @include media-breakpoint-down(sm){
+      .nav {
+        display: none;
+      }
     }
 }
 </style>
